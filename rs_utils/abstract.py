55f2e2bb280e7fc2697896c4a7f2b2f20967a10c
Alexander Kleinhans <rowsheet.com@gmail.com>
2019-08-14 23:05:15 -0600

    Adding test groups to be run with unit tests.

--- rs_utils/abstract.py
+++ rs_utils/abstract.py
@@ -1,3 +1,4 @@
+import os
 from rs_utils import logger 
 
 class RSObject(object):
@@ -5,28 +6,44 @@ class RSObject(object):
 	def __init__(self, test_file_path=None):
 		pass
 
-	def _test(self, test_file_path=None):
+	def _reinstall_module(self):
+		logger.message_big("Re-installing package modules...")
+		if os.path.exists("../install.sh") == False:
+			raise Exception(
+				"Unable to install package:" +
+				"../install.sh doesn't exist."
+			)
+		install_error = os.system("cd .. && sudo sh install.sh")
+		if install_error:
+			raise Exception("ERROR re-installing module.")
+		logger.success(
+			"Successfully re-installed package modules..."
+		)
+
+	def _validate_test_file_path(self, test_file_path):
+		if test_file_path is None:
+			raise Exception(
+				"Unable to run test: no test_file_path provided."
+			)
+
+	def _parse_groups(self, test_groups):
+		if test_groups is not None:
+			return ",".join(list(filter(lambda x: x != "", test_groups.split(","))))
+		return ""
+
+	def _test(self, test_file_path=None, test_groups=None):
 		try:
-			logger.message_big("Re-installing package modules...")
-			if test_file_path is None:
-				raise Exception(
-					"Unable to run test: no test_file_path provided."
+			self._reinstall_module()
+			self._validate_test_file_path(test_file_path)
+			parsed_groups = self._parse_groups(test_groups)
+			logger.message_big("Starting unit tests...")
+			logger.warning("GROUPS:")
+			logger.warning(parsed_groups)
+			os.system("python3 %s %s" % (
+					test_file_path,
+					parsed_groups,
 				)
-			import os
-			if os.path.exists("../install.sh") == False:
-				raise Exception(
-					"Unable to install package:" +
-					"../install.sh doesn't exist."
-				)
-			install_error = os.system("cd .. && sudo sh install.sh")
-			if install_error:
-				raise Exception("ERROR re-installing module.")
-			else:
-				logger.success(
-					"Successfully re-installed package modules..."
-				)
-				logger.message_big("Starting unit tests...")
-				os.system("python3 %s" % test_file_path)
+			)
 		except Exception as ex:
 			logger.error_big([
 				"Error re-installing module on unit test:",
--- rs_utils/unit_test.py
+++ rs_utils/unit_test.py
@@ -1,4 +1,5 @@
-from rs_utils import logger 
+import traceback
+from rs_utils import logger
 
 class TestResult:
 	PASSED = None
@@ -46,7 +47,11 @@ A unit test requires a number of things:
 	   This should be of type "function".
 		"auto_cleanup": lambda: [some auto-cleanup function]
 """
-def _run_test(test_name, test, test_function):
+def _run_test(test_name=None, test=None, test_function=None):
+	# logger.success("test_function:" + type(test_function).__name__)
+	# logger.success("test_name:" + type(test_name).__name__)
+	# logger.success("test:" + type(test).__name__)
+
 
 	logger.debug("%s: Running..." % test_name)
 
@@ -124,10 +129,64 @@ def _run_test(test_name, test, test_function):
 
 	return test_result
 
-def run(tests, test_function):
+def _parse_groups(groups, skip_first_arg=False):
+	start_index = 0
+	if type(groups).__name__ == "str":
+		return list(filter(lambda x: x != "", groups.split(",")))
+	if skip_first_arg == True:
+		start_index = 1
+	if groups is None:
+		return None
+	if skip_first_arg == True and len(groups) == 1:
+		return None
+	if len(groups) == (start_index + 1):
+		return list(filter(lambda x: x != "", groups[start_index].split(",")))
+	else:
+		return list(filter(lambda x: x != "", groups[start_index:]))
+
+def _test_in_group(test, groups, test_name):
+	test_groups = []
+	if "group" in test:
+		test_groups = _parse_groups(test["group"])
+	elif "groups" in test:
+		test_groups = _parse_groups(test["groups"])
+	for group in groups:
+		for test_group in test_groups:
+			if group == test_group:
+				return True
+	return False
+
+"""
+Running tests uses this function in unit test files.
+Test files should be able to be run by simply running:
+
+	python /path/to/test.py 
+
+Additionally, tests can be skipped by passing a test group or comma separated
+list of test groups to argv of the test file, i.e.
+
+	pytong /path/to/test.py group1,group2,group3
+
+If a group is passed, tests that do not have that group will
+be skipped.
+
+Tests can also be skipped if in the test definition, iether:
+
+	1) "skip": True
+	2) "run": False
+
+Tests are run with "skip" => False and "run" => True by default.
+"""
+def run(tests=None, test_function=None, argv=None):
 	summary = {}
+	groups = _parse_groups(argv, skip_first_arg=True)
 	try:
 		for test_name, test in tests.items():
+			"""
+			Determine if the test should be skipped. It should be skipped
+			if "run" => False, "skip" => True, or groups are not null and
+			the test is not in the group.
+			"""
 			skip = False
 			if "run" in test:
 				if test["run"] == False:
@@ -135,10 +194,25 @@ def run(tests, test_function):
 			if "skip" in test:
 				if test["skip"] == True:
 					skip = True
+			if (skip == False) and (groups is not None):
+				test_in_group = _test_in_group(test, groups, test_name)
+				if test_in_group == False:
+					skip = True
+			"""
+			Create the test result by either running the test or creating
+			a blank TestResult with SKIPPED=True, then append the test_result
+			to the summary so we can print them out later.
+			"""
+			test_result = None
 			if skip == True:
-				summary[test_name] = TestResult(SKIPPED=True)
+				test_result = TestResult(SKIPPED=True)
 			else:
-				summary[test_name] = _run_test(test_name, test, test_function)
+				test_result = _run_test(
+					test_name=test_name,
+					test=test,
+					test_function=test_function
+				)
+			summary[test_name] = test_result
 		logger.message_big("Test Summary:")
 		for test_name, test_result in summary.items():
 			if test_result.SKIPPED == True:
@@ -151,3 +225,232 @@ def run(tests, test_function):
 	except Exception as ex:
 		logger.error_big("Unable to run tests.")
 		logger.error(str(ex))
+		traceback.print_tb(ex.__traceback__)
+
+
+
+# Unit test
+if __name__ == "__main__":
+	from rs_utils import runner
+
+	def test_function(actual, should):
+		return TestResult(
+			PASSED=True
+		)
+
+	cmd_log = False
+	msg_log = False
+	stdout_log = False
+
+	tests = {
+		"TEST_SKIP_TRUE": {
+			"skip": True,
+			"actual": lambda: runner.step(
+				"echo TEST_SKIP_TRUE",
+				"Running TEST_SKIP_TRUE",
+				cmd_log = cmd_log,
+				msg_log = msg_log,
+				stdout_log = stdout_log,
+			),
+			"should": {
+				"ERROR": False
+			},
+		},
+		"TEST_SKIP_FALSE": {
+			"skip": False,
+			"actual": lambda: runner.step(
+				"echo TEST_SKIP_FALSE",
+				"Running TEST_SKIP_FALSE",
+				cmd_log = cmd_log,
+				msg_log = msg_log,
+				stdout_log = stdout_log,
+			),
+			"should": {
+				"ERROR": False
+			},
+		},
+		"TEST_RUN_TRUE": {
+			"run": True,
+			"actual": lambda: runner.step(
+				"echo TEST_RUN_TRUE",
+				"Running TEST_RUN_TRUE",
+				cmd_log = cmd_log,
+				msg_log = msg_log,
+				stdout_log = stdout_log,
+			),
+			"should": {
+				"ERROR": False
+			},
+		},
+		"TEST_RUN_FALSE": {
+			"run": False,
+			"actual": lambda: runner.step(
+				"echo TEST_RUN_FALSE",
+				"Running TEST_RUN_FALSE",
+				cmd_log = cmd_log,
+				msg_log = msg_log,
+				stdout_log = stdout_log,
+			),
+			"should": {
+				"ERROR": False
+			},
+		},
+		"TEST_GROUP_FOO": {
+			"actual": lambda: runner.step(
+				"echo TEST_GROUP_FOO",
+				"Running TEST_GROUP_FOO",
+				cmd_log = cmd_log,
+				msg_log = msg_log,
+				stdout_log = stdout_log,
+			),
+			"should": {
+				"ERROR": False
+			},
+			"group": "FOO",
+		},
+		"TEST_GROUP_BAR": {
+			"actual": lambda: runner.step(
+				"echo TEST_GROUP_BAR",
+				"Running TEST_GROUP_BAR",
+				cmd_log = cmd_log,
+				msg_log = msg_log,
+				stdout_log = stdout_log,
+			),
+			"should": {
+				"ERROR": False
+			},
+			"group": "BAR",
+		},
+		"TEST_GROUP_BAZ": {
+			"actual": lambda: runner.step(
+				"echo TEST_GROUP_BAZ",
+				"Running TEST_GROUP_BAZ",
+				cmd_log = cmd_log,
+				msg_log = msg_log,
+				stdout_log = stdout_log,
+			),
+			"should": {
+				"ERROR": False
+			},
+			"group": "BAZ",
+		},
+		"TEST_MULTI_GROUP_LIST": {
+			"actual": lambda: runner.step(
+				"echo TEST_MULTI_GROUP_LIST",
+				"Running TEST_MULTI_GROUP_LIST",
+				cmd_log = cmd_log,
+				msg_log = msg_log,
+				stdout_log = stdout_log,
+			),
+			"should": {
+				"ERROR": False
+			},
+			"group": ["FOO", "BAR", "BAZ"],
+		},
+		"TEST_MULTI_GROUP_CSV": {
+			"actual": lambda: runner.step(
+				"echo TEST_MULTI_GROUP_CSV",
+				"Running TEST_MULTI_GROUP_CSV",
+				cmd_log = cmd_log,
+				msg_log = msg_log,
+				stdout_log = stdout_log,
+			),
+			"should": {
+				"ERROR": False
+			},
+			"group": "FOO,BAR,BAZ",
+		},
+		"TEST_MULTI_GROUPS_LIST_NO_BAZ": {
+			"actual": lambda: runner.step(
+				"echo TEST_MULTI_GROUPS_LIST",
+				"Running TEST_MULTI_GROUPS_LIST",
+				cmd_log = cmd_log,
+				msg_log = msg_log,
+				stdout_log = stdout_log,
+			),
+			"should": {
+				"ERROR": False
+			},
+			"groups": ["FOO", "BAR"]
+		},
+		"TEST_MULTI_GROUPS_CSV_NO_BAZ": {
+			"actual": lambda: runner.step(
+				"echo TEST_MULTI_GROUPS_CSV",
+				"Running TEST_MULTI_GROUPS_CSV",
+				cmd_log = cmd_log,
+				msg_log = msg_log,
+				stdout_log = stdout_log,
+			),
+			"should": {
+				"ERROR": False
+			},
+			"groups": "FOO,BAR",
+		},
+	}
+
+	"""
+	Simulate argv:
+		python test.py
+	"""
+	logger.message_big("Testing Normal")
+	fake_argv = ["unit_test.py"]
+	run(
+		tests=tests,
+		test_function=test_function,
+		argv=fake_argv
+	)
+	"""
+	Simulate argv:
+		python test.py FOO,BAZ
+	"""
+	logger.message_big("Testing Multi Group (csv): FOO,BAZ")
+	fake_argv = ["unit_test.py", "FOO,BAZ,"]
+	run(
+		tests=tests,
+		test_function=test_function,
+		argv=fake_argv
+	)
+	"""
+	Simulate argv:
+		python test.py FOO BAR BAZ
+	"""
+	logger.message_big("Testing Multi Group (list): FOO BAR BAZ")
+	fake_argv = ["unit_test.py", "FOO", "BAR", "BAZ"]
+	run(
+		tests=tests,
+		test_function=test_function,
+		argv=fake_argv
+	)
+	"""
+	Simulate argv:
+		python test.py FOO
+	"""
+	logger.message_big("Testing Group: FOO")
+	fake_argv = ["unit_test.py", "FOO"]
+	run(
+		tests=tests,
+		test_function=test_function,
+		argv=fake_argv
+	)
+	"""
+	Simulate argv:
+		python test.py BAR 
+	"""
+	logger.message_big("Testing Group: BAR")
+	fake_argv = ["unit_test.py", "BAR"]
+	run(
+		tests=tests,
+		test_function=test_function,
+		argv=fake_argv
+	)
+	"""
+	Simulate argv:
+		python test.py BAZ 
+	"""
+	logger.message_big("Testing Group: BAZ")
+	fake_argv = ["unit_test.py", "BAZ"]
+	run(
+		tests=tests,
+		test_function=test_function,
+		argv=fake_argv
+	)
